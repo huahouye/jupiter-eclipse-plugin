d51ac4ef0f39d3d805a8030bf8835c78a4cfc76c
jsakuda <jsakuda@bc527f69-382e-0410-b1a7-611464369e04>
2008-02-11 10:20:26 +0000

    [Issue 11, Issue 17] Fixing 'jumpy' behavior when double-clicking on issues in the review table.

--- plugin/src/edu/hawaii/ics/csdl/jupiter/ui/marker/MarkerSelectionListener.java
+++ plugin/src/edu/hawaii/ics/csdl/jupiter/ui/marker/MarkerSelectionListener.java
@@ -2,12 +2,9 @@ package edu.hawaii.ics.csdl.jupiter.ui.marker;
 
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPart;
 
-import edu.hawaii.ics.csdl.jupiter.model.reviewissue.ReviewIssue;
 import edu.hawaii.ics.csdl.jupiter.ui.view.table.ReviewTableView;
 import edu.hawaii.ics.csdl.jupiter.ui.view.table.ReviewTableViewAction;
 
@@ -27,23 +24,30 @@ public class MarkerSelectionListener implements ISelectionListener {
    */
   public void selectionChanged(IWorkbenchPart part, ISelection selection) {
     if (selection instanceof ITextSelection) {
-      String lineNumber = String.valueOf(((ITextSelection) selection).getStartLine() + 1);
+      /*
+       * This method is trying move the selected issues to the top of the table.
+       * This is unnecessary and this method fails to correctly do this because 
+       * it bases the issue it selects on the line number contained in the issue.
+       * Multiple issue can be on the same line number so this fails to select the
+       * correct issue in those cases.
+       */
+//      String lineNumber = String.valueOf(((ITextSelection) selection).getStartLine() + 1);
       ReviewTableView[] tableViews = ReviewTableView.getViews();
       for (int i = 0; i < tableViews.length; i++) {
-        ReviewTableView tableView = tableViews[i];
-        if (tableView != null) {
-          Table table = tableView.getTable();
-          TableItem[] items = table.getItems();
-          for (int j = 0; j < items.length; j++) {
-            ReviewIssue reviewIssue = (ReviewIssue) items[j].getData();
-            if (lineNumber.equals(reviewIssue.getLine())) {
-              table.select(j);
-              table.setTopIndex(j);
-              break;
-            }
-          }
+//        ReviewTableView tableView = tableViews[i];
+//        if (tableView != null) {
+//          Table table = tableView.getTable();
+//          TableItem[] items = table.getItems();
+//          for (int j = 0; j < items.length; j++) {
+//            ReviewIssue reviewIssue = (ReviewIssue) items[j].getData();
+//            if (lineNumber.equals(reviewIssue.getLine())) {
+//              table.select(j);
+//              table.setTopIndex(j);
+//              break;
+//            }
+//          }
           ReviewTableViewAction.NOTIFY_EDITOR.run();
-        }
+//        }
       }
     }
   }
